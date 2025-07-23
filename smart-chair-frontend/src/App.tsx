52e7fd6e696d7e9a3adb119ccfd1dae381fd112e
Dennis Schreiber <boamah1357@gmail.com>
2025-07-23 17:12:07 +0200

    integrate tailwind

--- smart-chair-frontend/src/App.tsx
+++ smart-chair-frontend/src/App.tsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <div>
-        <h1>test frontend chair</h1>
+        <h1 className="text-4xl font-bold text-blue-600">
+          test frontend chair
+        </h1>
         <p>Visualisierung</p>
       </div>
     </>
